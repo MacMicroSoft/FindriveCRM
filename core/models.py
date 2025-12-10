78b0429b273e8cc906beaa7e25bbe3d3122a06a8
Alex Shymchuk <paintool1@gmail.com>
2025-12-10 23:51:03 +0200

    Created login and singup form with using allauth lib. Changed User model for allauth logic

--- core/models.py
+++ core/models.py
@@ -67,7 +67,7 @@ class User(AbstractBaseUser, AbstractTimeStampModel):
     is_blocked=models.BooleanField(default=False)
     last_login_at=models.DateTimeField(null=True, blank=True)
 
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
