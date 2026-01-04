93ae695ba8455f4ad483cc74e0b39278ed16a873
Alex Shymchuk <paintool1@gmail.com>
2026-01-04 22:34:34 +0200

    test

--- core/consumers.py
+++ core/consumers.py
@@ -1,12 +1,8 @@
 import json
-from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from . services import create_message, creat_chat, get_chat
 from .models import SenderChoice
-from .tasks import handle_ws_message
 import uuid
-import asyncio
-import aio_pika
 
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
@@ -17,29 +13,8 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             "text": content["text"],
             "sender": content["sender"]
         }
-        await asyncio.sleep(5)
-        handle_ws_message.delay(payload)
 
         await self.send_json({
             "status": "queued",
             "message_uuid": payload["message_uuid"]
         })
-
-
-async def web_rabbit_listener():
-    while True:
-        connection = await aio_pika.connect_robust("amqp://guest:guest@localhost:5672/")
-        queue_name = "tg_to_web"
-
-        async with connection:
-            channel = await connection.channel()
-            queue = await channel.declare_queue(queue_name, durable=True)
-
-            async with queue.iterator() as queue_iter:
-                async for message in queue_iter:
-                    async with message.process():
-                        payload = message.body.decode()
-                        data = json.loads(payload)[0][0]
-                        print(payload)
-
-asyncio.run(web_rabbit_listener())
